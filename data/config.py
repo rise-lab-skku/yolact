3c7468f0fa66be64eb150bb93959dbc25fd5e1e2
ohilho <ohilho@g.skku.edu>
2021-10-17 09:20:51 +0000

    include zero values again. remove comments

--- data/config.py
+++ data/config.py
@@ -368,16 +368,9 @@ unloader_rgbd_dataset = unloader_rgbd_dataset_base.copy(
         "test_info": "./data/coco/unloader/unloader_rgbd_20210930/info/test.json",
         # augmentation settings
         "augment_random_sample_crop": False,
-        # mean and std of rgbd
-        # median and std
-        # "mean": [124.20291064, 119.6903013, 114.94422638, 0.0],
-        # "std": [71.05941776, 73.38871724, 74.34486907, 697.6756],
-        # mean and std with only non zero data
-        "mean": [124.20291064, 119.6903013, 114.94422638, 2082.3953],
-        "std": [71.05941776, 73.38871724, 74.34486907, 697.6756],
         # real mean and std
-        # "mean": [124.20291064, 119.6903013, 114.94422638, 578.37429271],
-        # "std": [71.05941776, 73.38871724, 74.34486907, 1002.53661683],
+        "mean": [124.20291064, 119.6903013, 114.94422638, 578.37429271],
+        "std": [71.05941776, 73.38871724, 74.34486907, 1002.53661683],
     }
 )
 
